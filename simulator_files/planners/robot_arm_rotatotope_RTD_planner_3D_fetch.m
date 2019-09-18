91c3898837aab234c77b77b5b34b43942c2d6fba
pdholmes <pdholmes@umich.edu>
2019-09-18 18:24:55 -0400

    first draft of rotatotope RTD seems to be working

--- simulator_files/planners/robot_arm_rotatotope_RTD_planner_3D_fetch.m
+++ simulator_files/planners/robot_arm_rotatotope_RTD_planner_3D_fetch.m
@@ -21,7 +21,7 @@ classdef robot_arm_rotatotope_RTD_planner_3D_fetch < robot_arm_generic_planner
     methods
         function P = robot_arm_rotatotope_RTD_planner_3D_fetch(varargin)
             t_move = 0.5;
-            lookahead_distance = 0.1;
+            lookahead_distance = 0.3;
 %             HLP = robot_arm_RRT_HLP('make_new_tree_every_iteration_flag',true) ;
 %             HLP = robot_arm_PRM_HLP( ) ;
             HLP = robot_arm_straight_line_HLP( );
@@ -63,11 +63,9 @@ classdef robot_arm_rotatotope_RTD_planner_3D_fetch < robot_arm_generic_planner
             
             %%% 4. generate desired trajectory
             if ~trajopt_failed
-                P.vdisp('New trajectory found!',3)
-                T = 0:P.time_discretization:P.t_total ;
-                N_T = length(T) ;
-                U = zeros(P.arm_n_inputs,N_T) ;
-                Z = P.generate_trajectory(T, q_0, q_dot_0, k_opt);
+                P.vdisp('New trajectory found!',3);
+                
+                [T, U, Z] = P.generate_trajectory(q_0, q_dot_0, k_opt);
                 
                 toc(planning_time);
              
@@ -107,7 +105,7 @@ classdef robot_arm_rotatotope_RTD_planner_3D_fetch < robot_arm_generic_planner
             %             toc;
         end
         
-        function [k_opt] = trajopt(P, q_0, q_dot_0, q_des)
+        function [k_opt, trajopt_failed] = trajopt(P, q_0, q_dot_0, q_des)
             % use fmincon to optimize the cost subject to constraints
             P.vdisp('Running trajopt',3)
             
@@ -115,21 +113,27 @@ classdef robot_arm_rotatotope_RTD_planner_3D_fetch < robot_arm_generic_planner
             constraint_func = @(k) P.eval_constraint(k);
             
             % generate upper and lower bounds
-            lb = [];
-            ub = [];
-            for i = 1:size(k_lim{end}.V, 1)
-                lb = [lb; min(k_lim{end}.V(i, :))];
-                ub = [ub; max(k_lim{end}.V(i, :))];
-            end
+%             lb = [];
+%             ub = [];
+%             for i = 1:size(k_lim{end}.V, 1)
+%                 lb = [lb; min(k_lim{end}.V(i, :))];
+%                 ub = [ub; max(k_lim{end}.V(i, :))];
+%             end
+            lb = P.R.c_k - P.R.g_k;
+            ub = P.R.c_k + P.R.g_k;
             
             initial_guess = (lb + ub)/2;
            
 %             options = optimoptions('fmincon','SpecifyConstraintGradient',true, 'Algorithm', 'interior-point');
-            options = optimoptions('fmincon','SpecifyConstraintGradient',true);
+%             options = optimoptions('fmincon','SpecifyConstraintGradient',true);
+            options = optimoptions('fmincon');
             [k_opt, ~, exitflag, ~] = fmincon(cost_func, initial_guess, [], [], [], [], lb, ub, constraint_func, options) ;
             
             if exitflag <= 0
-                error('planner:trajOpt', 'Solver did not converge.');
+%                 error('planner:trajOpt', 'Solver did not converge.');
+                trajopt_failed = true;
+            else
+                trajopt_failed = false;
             end
         end
         
@@ -139,39 +143,76 @@ classdef robot_arm_rotatotope_RTD_planner_3D_fetch < robot_arm_generic_planner
            cost = sum((q_plan - q_des).^2);
         end
         
-        function [c, ceq, gradc, gradceq] = eval_constraint(P, k_opt)
+        function [c, ceq] = eval_constraint(P, k_opt)
             epsilon = 1e-3;
             ceq = [];
-            gradceq = [];
+%             gradceq = [];
             
             c = [];
-            gradc = [];
+%             gradc = [];
             %%% Obstacle constraint generation:
-            for i = 1:length(k_unsafe_A) % for each link
-                for j = 1:length(k_unsafe_A{i}) % for each obstacle
-                    idx = find(~cellfun('isempty', k_unsafe_A{i}{j}));
-%                     for k = 1:length(k_unsafe_A{i}{j}) % for each time step
-                    for k = 1:length(idx)
-                        c_obs = k_unsafe_A{i}{j}{idx(k)}*k_opt - k_unsafe_b{i}{j}{idx(k)};
+            for i = 1:length(P.R.A_con) % for each obstacle
+                for j = 1:length(P.R.A_con{i}) % for each link
+                    idx = find(~cellfun('isempty', P.R.A_con{i}{j}));
+                    k_param = k_opt(P.R.link_joints{j});
+                    c_param = P.R.c_k(P.R.link_joints{j});
+                    g_param = P.R.g_k(P.R.link_joints{j});
+                    lambda = c_param + (k_param./g_param);
+                    for k = 1:length(idx) % for each time step
+                        lambdas = P.R.k_con{i}{j}{idx(k)}.*lambda;
+                        lambdas(~lambdas) = 1;
+                        lambdas = prod(lambdas, 1)';
+                        
+                        c_obs = P.R.A_con{i}{j}{idx(k)}*lambdas - P.R.b_con{i}{j}{idx(k)};
                         c_obs_max = max(c_obs);
                         c_k = -(c_obs_max - epsilon);
                         c = [c; c_k];
                         
                         % specify gradients
-                        if nargout > 2
-                            maxidx = find(c_obs == c_obs_max);
-                            if length(maxidx) > 1
-%                                 disp('ahhh');
-                                tempgradc = k_unsafe_A{i}{j}{idx(k)}(maxidx, :);
-                                gradc = [gradc, -max(tempgradc)'];
-                            else
-                                gradc = [gradc, -k_unsafe_A{i}{j}{idx(k)}(maxidx, :)'];
-                            end
-                        end
+                        % PATRICK need to fill this out.
+%                         if nargout > 2
+%                             maxidx = find(c_obs == c_obs_max);
+%                             if length(maxidx) > 1
+% %                                 disp('ahhh');
+%                                 tempgradc = k_unsafe_A{i}{j}{idx(k)}(maxidx, :);
+%                                 gradc = [gradc, -max(tempgradc)'];
+%                             else
+%                                 gradc = [gradc, -k_unsafe_A{i}{j}{idx(k)}(maxidx, :)'];
+%                             end
+%                         end
+                        
                     end
                 end
             end
             
+        end
+        
+        function [T, U, Z] = generate_trajectory(P, q_0, q_dot_0, k_opt)
+            T = 0:P.time_discretization:P.t_total ;
+            T_plan = T(1:floor(length(T)/2)+1) ;
+            T_brake = T(floor(length(T)/2)+1:end) ;
+            
+            t_to_stop = T_brake(end) - T_brake(1);
+            U = zeros(P.arm_n_inputs,length(T));
+            
+            q_to_peak = q_0 + q_dot_0.*T_plan + (1/2)*k_opt.*T_plan.^2;
+            q_dot_to_peak = q_dot_0 + k_opt.*T_plan;
+            
+            q_peak = q_to_peak(:, end);
+            q_dot_peak = q_dot_to_peak(:, end);
+            
+            T_brake = T_brake - T_brake(1);
+            q_to_stop = q_peak + q_dot_peak.*T_brake + (1/2)*((0 - q_dot_peak)./t_to_stop).*T_brake.^2;
+            q_dot_to_stop = q_dot_peak + ((0 - q_dot_peak)./t_to_stop).*T_brake;
+            
+            % remove overlap
+            q_to_stop(:, 1) = [];
+            q_dot_to_stop(:, 1) = [];
+            
+            Z = zeros(length(q_0)*2, length(T));
+            for i = 1:size(q_to_peak, 1)
+               Z(2*i-1:2*i, :) =  [q_to_peak(i, :), q_to_stop(i, :); q_dot_to_peak(i, :), q_dot_to_stop(i, :)];
+            end
         end
         
         function [q_plan] = compute_q_plan(P, q_0, q_dot_0, k)
