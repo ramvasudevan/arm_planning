4d305413b6e6effbcc0bed3df903f1e41f3862a0
Raquelometer <daphnaraz@gmail.com>
2019-12-27 22:04:02 -0500

    final version of armtd benchmarking with Chomp

--- armtd_benchmark/chomp_processing/chomp_processing.py
+++ armtd_benchmark/chomp_processing/chomp_processing.py
@@ -6,18 +6,18 @@ def main():
 
 	file_path = "processed/"
 
-	filename_in = sys.argv[1]
+	filename_in = "raw/" + sys.argv[1]
 	test_data = open(filename_in, 'r')
 
-	num_obstacles = filename_in[15] + filename_in[16]
+	num_obstacles = filename_in[15] + filename_in[16] + filename_in[17]
 	print num_obstacles
 
-	world = filename_in[18] + filename_in[19]
+	world = filename_in[19] + filename_in[20] + filename_in[21]
 	print world 
 
 
 	#filename_out = "chomp_test_results_" + num_obstacles + world + ".txt"
-	filename_out = file_path + "chomp_test_results_" + num_obstacles + world + ".csv"
+	filename_out = file_path + "chomp_test_results_" + num_obstacles + "_" + world + ".csv"
 
 	print filename_out
 
@@ -29,8 +29,11 @@ def main():
 	lines_in = test_data.readlines() 
 	row = []
 	
+	reorder = 0
 
 	for line in lines_in:
+
+		#row = []
 
 		if "Chomp path is collision free" in line:
 			print "SUCCESS!"
@@ -43,6 +46,7 @@ def main():
 			print "FAILURE!"
 			#output_data.write("FAILURE")
 			row.append(0)
+			reorder = 1
 
 		if "Optimization core" in line:
 			words = line.split("finished in")
@@ -64,9 +68,15 @@ def main():
 			print time 
 			#output_data.write(time)
 			row.append(time)
-	print "ROW"
-	print row
+		#if len(row) != 0:
+			#output_csv.writerow(row)	
+	if reorder:
+		# slice out 0, insert it at idx= 3
+		row.pop(0)
+		row.insert(3,0)
+
 	output_csv.writerow(row)
+
 	return
 
 if __name__ == '__main__':
--- armtd_benchmark/run_batch_tests.sh
+++ armtd_benchmark/run_batch_tests.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Run an entire batch of tests for x obstacles
+# If you have more than 10 tests, adjust the number
+# range in the for loop
+
+# Usage: ./run_batch_tests.sh <num_obstacles>
+
 num_obstacles=$1
 
-for test in {1..3}
+for test in {1..10}
 do
 	./run_test.sh $num_obstacles $test
+	sleep 2
 done
+
+
