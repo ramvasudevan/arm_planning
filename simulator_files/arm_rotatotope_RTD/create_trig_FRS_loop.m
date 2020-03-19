8116cd13cf01b16c8f9b9c62d70293ca14e1d98f
pdholmes <pdholmes@umich.edu>
2020-03-19 13:18:34 -0400

    Update create_trig_FRS_loop.m

--- simulator_files/arm_rotatotope_RTD/create_trig_FRS_loop.m
+++ simulator_files/arm_rotatotope_RTD/create_trig_FRS_loop.m
@@ -1,22 +1,26 @@
-% playing around with matZonotopes...
+%%% description
+% this code uses CORA 2018 to generate the "joint reachable sets" of
+% cosines and sines that are parameterized by a parameter k
+% in this case, we're solving for reachable sines and cosines over a range
+% of initial velocities, and saving many reach sets corresponding to each
+% range of velocities. we are also varying the set K from which k is drawn,
+% depending on the set of initial velocities.
 
 clear all; clc;
 figure(1); clf; hold on; axis equal;
 
-dim = 5;
+dim = 5; % dim 1 cosine, dim 2 sine, dim 3 K, dim 4 initial vel., dim 5 time
 
 t_plan = 0.5;
 t_total = 1;
 dt = 0.01;
 
-% c_IC = pi/2;
-% g = pi/6;
 L = 0.33;
 
-generate_trig_dynamics(t_plan, t_total)
+generate_trig_dynamics(t_plan, t_total) % generates the dynamics parameterized by k
 
-nBins = 401;
-c_IC = linspace(-pi, pi, nBins);
+nBins = 401; % separate initial velocity space into 401 smaller intervals
+c_IC = linspace(-pi, pi, nBins); % initial velocity in [-pi, pi]
 binSize = c_IC(2) - c_IC(1);
 g_IC = binSize/2;
 
@@ -26,7 +30,7 @@ if ~exist('FRS_trig', 'dir')
 end
 save('FRS_trig/0key.mat', 'c_IC');
 
-for i = 1:length(c_IC)
+for i = 1:length(c_IC) % we're going to loop over all velocity intervals
     options.tStart = 0;
     options.tFinal = t_plan;
     
@@ -63,9 +67,9 @@ for i = 1:length(c_IC)
     options.R0 = zonotope_slice(options.R0, [dim], t_plan);
     options.tStart = t_plan;
     options.tFinal = t_total;
-    %specify 1st link to stop dynamics------------------------------------------
+    %specify the "to stop" aka braking dynamics----------------------------
     sys = nonlinearSys(dim, 1, @trig_dyn_toStop, options);
-    %compute reachable set-----------------------------------------------------
+    %compute reachable set-------------------------------------------------
     tic
     Rcont_toStop = reach(sys, options);
     tComp = toc;
@@ -73,6 +77,7 @@ for i = 1:length(c_IC)
     % concatenate full FRS
     Rcont = [Rcont_toPeak; Rcont_toStop];
     
+    % save this FRS
     my_c_IC = c_IC(i);
     filename = sprintf('FRS_trig/trig_FRS_%0.3f.mat', my_c_IC);
     save(filename, 'Rcont', 'options', 'L', 't_plan', 't_total', 'my_c_IC');
